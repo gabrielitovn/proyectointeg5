af2df3785ad5b5e4a445e4a4b1d4ac2fbe070f60
gabrielitovn <139518424+gabrielitovn@users.noreply.github.com>
2023-11-09 14:19:34 -0600

    Add files via upload

---
+++ juego.py
@@ -0,0 +1,56 @@
+from pynput import keyboard as bk
+import os
+
+class Juego:
+
+    def __init__(self, inicial, final, laberinto) -> None:
+        self.inicial = inicial
+        self.final = final
+        self.laberinto = laberinto
+        listener = bk.Listener(on_release=self.on_key_release)
+        listener.start()
+        self.mostrarLaberinto()
+        listener.join()
+
+    posicion_personaje = [0, 0]
+
+    
+    def mostrarLaberinto():
+        # Limpia la pantalla
+        os.system('cls' if os.name == 'nt' else 'clear')
+        for i in range(fila):
+            for x in range(columna):
+                if posicion_personaje == [i, x]:
+                    print("P", end="")
+                else:
+                    print(laberinto[i][x], end="")
+            print("")
+
+
+    def moverPersonaje(self, key):
+        global posicion_personaje
+        nueva_posicion = list(posicion_personaje)
+        if key == bk.Key.up:
+            nueva_posicion[0] -= 1
+        elif key == bk.Key.down:
+            nueva_posicion[0] += 1
+        elif key == bk.Key.left:
+            nueva_posicion[1] -= 1
+        elif key == bk.Key.right:
+            nueva_posicion[1] += 1
+        # Verificar si la nueva posición es válida
+        if (0 <= nueva_posicion[0] < fila
+            and 0 <= nueva_posicion[1] < columna
+            and laberinto[nueva_posicion[0]][nueva_posicion[1]] == "."):
+            posicion_personaje = nueva_posicion
+            self.mostrarLaberinto()
+            self.verificar_final()
+
+    def verificar_final():
+        global fila, columna, posicion_personaje
+        if posicion_personaje == [fila - 1, columna - 1]:
+            exit()  # Finaliza el programa
+
+    def on_key_release(self, key):
+        if key in [bk.Key.up, bk.Key.down, bk.Key.left, bk.Key.right]:
+            self.moverPersonaje(key)
